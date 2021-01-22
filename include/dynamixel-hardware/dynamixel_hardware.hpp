6291a8973ebd71bdb7efee77ad8fdcf9d76ce872
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2021-01-22 16:59:39 +0900

    add include

---
+++ include/dynamixel-hardware/dynamixel_hardware.hpp
@@ -0,0 +1,29 @@
+// Copyright 2020 Yutaka Kondo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DYNAMIXEL_HARDWARE__DYNAMIXEL_HARDWARE_HPP_
+#define DYNAMIXEL_HARDWARE__DYNAMIXEL_HARDWARE_HPP_
+
+#include "hardware_interface/base_interface.hpp"
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "hardware_interface/types/hardware_interface_status_values.hpp"
+
+namespace dynamixel_hardware
+{
+}  // namespace dynamixel_hardware
+
+#endif  // DYNAMIXEL_HARDWARE__DYNAMIXEL_HARDWARE_HPP_
