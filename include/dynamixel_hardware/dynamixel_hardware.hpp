45ba393924de75cb99a6711e3008a71badc81e43
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2021-01-29 21:05:57 +0900

    succeed in build

--- include/dynamixel_hardware/dynamixel_hardware.hpp
+++ include/dynamixel_hardware/dynamixel_hardware.hpp
@@ -15,22 +15,64 @@
 #ifndef DYNAMIXEL_HARDWARE__DYNAMIXEL_HARDWARE_HPP_
 #define DYNAMIXEL_HARDWARE__DYNAMIXEL_HARDWARE_HPP_
 
+#include <dynamixel_workbench_toolbox/dynamixel_workbench.h>
+
 #include <hardware_interface/base_interface.hpp>
 #include <hardware_interface/handle.hpp>
 #include <hardware_interface/hardware_info.hpp>
 #include <hardware_interface/system_interface.hpp>
 #include <hardware_interface/types/hardware_interface_return_values.hpp>
 #include <hardware_interface/types/hardware_interface_status_values.hpp>
+#include <map>
+#include <vector>
 
 #include "dynamixel_hardware/visiblity_control.h"
+#include "rclcpp/macros.hpp"
+
+using hardware_interface::return_type;
 
 namespace dynamixel_hardware
 {
+struct Joint
+{
+  std::string name;
+  double command{0.0};
+  double position{0.0};
+  double velocity{0.0};
+  double effort{0.0};
+};
+
 class DynamixelHardware
 : public hardware_interface::BaseInterface<hardware_interface::SystemInterface>
 {
 public:
-  // RCLCPP_SHARED_PTR_DEFINITIONS(DynamixelHardware);
+  RCLCPP_SHARED_PTR_DEFINITIONS(DynamixelHardware)
+
+  DYNAMIXEL_HARDWARE_PUBLIC
+  return_type configure(const hardware_interface::HardwareInfo & info) override;
+
+  DYNAMIXEL_HARDWARE_PUBLIC
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  DYNAMIXEL_HARDWARE_PUBLIC
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  DYNAMIXEL_HARDWARE_PUBLIC
+  return_type start() override;
+
+  DYNAMIXEL_HARDWARE_PUBLIC
+  return_type stop() override;
+
+  DYNAMIXEL_HARDWARE_PUBLIC
+  return_type read() override;
+
+  DYNAMIXEL_HARDWARE_PUBLIC
+  return_type write() override;
+
+private:
+  DynamixelWorkbench dynamixel_workbench_;
+  std::map<const char * const, const ControlItem *> control_items_;
+  std::vector<Joint> joints_;
 };
 }  // namespace dynamixel_hardware
 
--- src/dynamixel_hardware.cpp
+++ src/dynamixel_hardware.cpp
@@ -14,6 +14,113 @@
 
 #include "dynamixel_hardware/dynamixel_hardware.hpp"
 
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "rclcpp/rclcpp.hpp"
+
 namespace dynamixel_hardware
 {
+constexpr const char * kDynamixelHardware = "DynamixelHardware";
+constexpr uint8_t kGoalPositionIndex = 0;
+constexpr uint8_t kPresentPositionVelocityCurrentIndex = 0;
+constexpr const char * kGoalPositionItem = "Goal_Position";
+constexpr const char * kGoalVelocityItem = "Goal_Velocity";
+constexpr const char * kMovingSpeedItem = "Moving_Speed";
+constexpr const char * kPresentPositionItem = "Present_Position";
+constexpr const char * kPresentVelocityItem = "Present_Velocity";
+constexpr const char * kPresentSpeedItem = "Present_Speed";
+constexpr const char * kPresentCurrentItem = "Present_Current";
+constexpr const char * kPresentLoadItem = "Present_Load";
+constexpr int kDefaultBaudRate = 57600;
+
+return_type DynamixelHardware::configure(const hardware_interface::HardwareInfo & info)
+{
+  if (configure_default(info) != return_type::OK) {
+    return return_type::ERROR;
+  }
+
+  joints_.resize(info_.joints.size(), Joint());
+
+  // for (const hardware_interface::ComponentInfo & joint : info_.joints) {
+  // }
+
+  status_ = hardware_interface::status::CONFIGURED;
+  return return_type::OK;
+}
+
+std::vector<hardware_interface::StateInterface> DynamixelHardware::export_state_interfaces()
+{
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+  for (uint i = 0; i < info_.joints.size(); i++) {
+    state_interfaces.emplace_back(hardware_interface::StateInterface(
+      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &joints_[i].position));
+  }
+
+  return state_interfaces;
+}
+
+std::vector<hardware_interface::CommandInterface> DynamixelHardware::export_command_interfaces()
+{
+  std::vector<hardware_interface::CommandInterface> command_interfaces;
+  for (uint i = 0; i < info_.joints.size(); i++) {
+    command_interfaces.emplace_back(hardware_interface::CommandInterface(
+      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &joints_[i].command));
+  }
+
+  return command_interfaces;
+}
+
+return_type DynamixelHardware::start()
+{
+  RCLCPP_INFO(rclcpp::get_logger(kDynamixelHardware), "Starting ...please wait...");
+
+  for (uint i = 0; i < joints_.size(); i++) {
+    if (std::isnan(joints_[i].position)) {
+      joints_[i].position = 0.0;
+      joints_[i].command = 0.0;
+    }
+  }
+
+  status_ = hardware_interface::status::STARTED;
+
+  RCLCPP_INFO(rclcpp::get_logger(kDynamixelHardware), "System Sucessfully started!");
+
+  return return_type::OK;
+}
+
+return_type DynamixelHardware::stop()
+{
+  RCLCPP_INFO(rclcpp::get_logger(kDynamixelHardware), "Stopping ...please wait...");
+
+  status_ = hardware_interface::status::STOPPED;
+
+  RCLCPP_INFO(rclcpp::get_logger(kDynamixelHardware), "System sucessfully stopped!");
+
+  return return_type::OK;
+}
+
+hardware_interface::return_type DynamixelHardware::read()
+{
+  RCLCPP_INFO(rclcpp::get_logger(kDynamixelHardware), "Reading...");
+
+  for (uint i = 0; i < joints_.size(); i++) {
+  }
+  RCLCPP_INFO(rclcpp::get_logger(kDynamixelHardware), "Joints sucessfully read!");
+
+  return return_type::OK;
+}
+
+hardware_interface::return_type dynamixel_hardware::DynamixelHardware::write()
+{
+  RCLCPP_INFO(rclcpp::get_logger(kDynamixelHardware), "Writing...");
+
+  for (uint i = 0; i < joints_.size(); i++) {
+  }
+  RCLCPP_INFO(rclcpp::get_logger(kDynamixelHardware), "Joints sucessfully written!");
+
+  return return_type::OK;
+}
 }  // namespace dynamixel_hardware
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(dynamixel_hardware::DynamixelHardware, hardware_interface::SystemInterface)
