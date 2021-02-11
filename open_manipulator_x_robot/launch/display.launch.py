491996ea3369a656a3c9771087f407b3654bc575
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2021-02-11 14:57:05 +0900

    succeed in launch

--- open_manipulator_x_robot/launch/display.launch.py
+++ open_manipulator_x_robot/launch/display.launch.py
@@ -25,28 +25,28 @@ import xacro
 
 def generate_launch_description():
     robot_description = os.path.join(get_package_share_directory(
-        "open_manipulator_x_robot"), "description", "open_manipulator_x_robot.urdf.xacro")
+        "open_manipulator_x_robot"), "urdf", "open_manipulator_x_robot.urdf.xacro")
     robot_description_config = xacro.process_file(robot_description)
 
     return LaunchDescription([
         Node(
             package="joint_state_publisher_gui",
-            node_executable="joint_state_publisher_gui",
-            node_name="joint_state_publisher_gui",
+            executable="joint_state_publisher_gui",
+            name="joint_state_publisher_gui",
             parameters=[{"source_list": ["joint_states"]},
                         {"robot_description": robot_description_config.toxml()}],
             output="screen"),
 
         Node(
             package="robot_state_publisher",
-            node_executable="robot_state_publisher",
-            node_name="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
             arguments=[robot_description],
             output="screen"),
 
         Node(
             package="rviz2",
-            node_executable="rviz2",
-            node_name="rviz2",
+            executable="rviz2",
+            name="rviz2",
             output="screen")
     ])
