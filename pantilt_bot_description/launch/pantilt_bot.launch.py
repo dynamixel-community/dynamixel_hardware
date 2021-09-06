07fd3f3410bf809d8ae1a7f0d3ef7edfcf7c6ba9
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2021-09-06 22:44:59 +0900

    update controllers

--- pantilt_bot_description/launch/pantilt_bot.launch.py
+++ pantilt_bot_description/launch/pantilt_bot.launch.py
@@ -18,7 +18,6 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
 
 import xacro
 
