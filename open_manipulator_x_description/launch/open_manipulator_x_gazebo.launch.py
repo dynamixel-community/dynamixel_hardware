939114ba0fdb2c7c2eaffbbf632e486809849b67
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2021-09-11 10:33:13 +0900

    enable open_manipulator_x_gazebo

--- open_manipulator_x_description/launch/open_manipulator_x_gazebo.launch.py
+++ open_manipulator_x_description/launch/open_manipulator_x_gazebo.launch.py
@@ -12,11 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
+import os.path
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 import xacro
@@ -31,39 +34,57 @@ def generate_launch_description():
         package_name), "urdf", robot_name + ".urdf.xacro")
     robot_description_config = xacro.process_file(robot_description)
 
-    controller_config = os.path.join(
-        get_package_share_directory(
-            package_name), "controllers", "controllers.yaml"
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-topic", "robot_description",
+                   "-entity", "open_manipulator_x"],
+        output="screen"
+    )
+
+    joint_state_broadcaster = ExecuteProcess(
+        cmd=["ros2", "control", "load_controller", "--set-state", "start", "--spin-time", "120",
+             "joint_state_broadcaster"],
+        output="screen"
+    )
+
+    joint_trajectory_controller = ExecuteProcess(
+        cmd=["ros2", "control", "load_controller", "--set-state", "start",
+             "joint_trajectory_controller"],
+        output="screen"
+    )
+
+    velocity_controller = ExecuteProcess(
+        cmd=["ros2", "control", "load_controller", "--set-state", "configure",
+             "velocity_controller"],
+        output="screen"
     )
 
     return LaunchDescription([
-        Node(
-            package="controller_manager",
-            executable="ros2_control_node",
-            parameters=[
-                {"robot_description": robot_description_config.toxml()}, controller_config],
-            output={
-                "stdout": "screen",
-                "stderr": "screen",
-            },
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[joint_state_broadcaster],
+            )
         ),
 
-        Node(
-            package="controller_manager",
-            executable="spawner.py",
-            arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster,
+                on_exit=[joint_trajectory_controller],
+            )
         ),
 
-        Node(
-            package="controller_manager",
-            executable="spawner.py",
-            arguments=["velocity_controller", "-c", "/controller_manager"],
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_trajectory_controller,
+                on_exit=[velocity_controller],
+            )
         ),
 
-        Node(
-            package="controller_manager",
-            executable="spawner.py",
-            arguments=["joint_trajectory_controller", "-c", "/controller_manager"],
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory("gazebo_ros"), "launch"), "/gazebo.launch.py"]),
         ),
 
         Node(
@@ -73,6 +94,8 @@ def generate_launch_description():
             parameters=[
                 {"robot_description": robot_description_config.toxml()}],
             output="screen"),
+
+        spawn_entity,
 
         Node(
             package="rviz2",
